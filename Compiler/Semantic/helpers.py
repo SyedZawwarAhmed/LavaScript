e9e6b15b6008f664651c86ae236a08aae1d3322f
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-12-03 02:17:46 +0500

    update 3 files and delete 1 file

--- Compiler/Semantic/helpers.py
+++ Compiler/Semantic/helpers.py
@@ -1,9 +1,10 @@
 from typing import List
-from data_table_row import Data_Table_Row
+from symbol_table import *
 from main_table_row import Main_Table_Row
-from tables import *
+from function_table_row import Function_Table_Row
+from data_table_row import Data_Table_Row
 
-def create_data_table(name, type, access_modifier, category, parent, link) -> List[Data_Table_Row]:
+def create_data_table() -> List[Data_Table_Row]:
     new_table: List[Data_Table_Row] = []
     return new_table
 
@@ -16,10 +17,20 @@ def insert_main_table(name, type, access_modifier, type_modifier, category, pare
     return True
 
 def insert_function_table(name, type, scope) -> bool:
-    return False
+    for row in function_table:
+        if row.name == name and row.scope == scope:
+            return False
+    new_row = Function_Table_Row(name, type, scope)
+    function_table.append(new_row)
+    return True
 
-def insert_data_table(name, type, access_modifier, type_modifier) -> bool:
-    return False
+def insert_data_table(name, type, access_modifier, type_modifier, data_table) -> bool:
+    for row in data_table:
+        if row.name == name:
+            return False
+    new_row = Data_Table_Row(name, type, access_modifier, type_modifier)
+    data_table.append(new_row)
+    return True
 
 def lookup_main_table():
     return
@@ -33,14 +44,18 @@ def lookup_function_data_table(name, scope):
 def lookup_funtion_table():
     return
 
-def compatibility_for_two_operands(left_operand_type, right_operand_type):
+def compatibility_for_two_operands(left_operand_type, right_operand_type, operator):
     return
 
 def compatibility_for_single_operand(operand_type):
     return
 
 def create_scope():
+    global current_scope
+    current_scope += 1
+    scope_stack.append(current_scope)
     return
 
 def destroy_scope():
+    scope_stack.pop()
     return
---
+++ Compiler/Semantic/symbol_table.py
@@ -0,0 +1,8 @@
+from typing import List
+from main_table_row import Main_Table_Row
+from function_table_row import Function_Table_Row
+
+main_table: List[Main_Table_Row] = []
+function_table: List[Function_Table_Row] = []
+current_scope = 0
+scope_stack = []
