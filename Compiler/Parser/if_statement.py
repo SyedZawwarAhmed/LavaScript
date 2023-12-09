86ecf123526c9ab475d643f5152b92593154b163
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-12-09 17:28:30 +0500

    update if_statement.py

--- Compiler/Parser/if_statement.py
+++ Compiler/Parser/if_statement.py
@@ -12,8 +12,8 @@ def if_statement() -> bool:
         if match_terminal(IF):
             if match_terminal(OPENING_PARENTHESIS):
                 type_of_expression = OE()
-                if type_of_expression:
-                    if type_of_expression != 'boolean':
+                if type_of_expression and type(type_of_expression) == Function_Table_Row_Type:
+                    if type_of_expression.type != 'boolean':
                         print("If condition must be of type boolean.")
                         return False
                     if match_terminal(CLOSING_PARENTHESIS):
